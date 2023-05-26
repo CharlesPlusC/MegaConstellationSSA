40e0cfb72738817eb3a013d8656a88af9af02476
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2023-05-26 14:16:46 +0100

    split workflow in two

--- source/tests/test_analysis_tools.py
+++ source/tests/test_analysis_tools.py
@@ -1,5 +1,5 @@
 """Pytest functions"""
 import pytest
-from tools.analysis_tools import TLE_rate_dicts, NORAD_vs_SUP_TLE_analysis, TLE_analysis_to_df, launch_specific_stats, sup_gp_op_benchmark, TLE_arglat_dict
+from ..tools.analysis_tools import TLE_rate_dicts, NORAD_vs_SUP_TLE_analysis, TLE_analysis_to_df, launch_specific_stats, sup_gp_op_benchmark, TLE_arglat_dict
 
 pass
